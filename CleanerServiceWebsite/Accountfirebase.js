ed49e3ffb18d3be5d1227567bda7cc40f2a440cc
DilaShineNair <87301230+DilaShineNair@users.noreply.github.com>
2021-07-15 03:02:46 +0800

    Add files via upload

---
+++ CleanerServiceWebsite/Accountfirebase.js
@@ -0,0 +1,11 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyD4Qboyl808OeNpAdfYd6libqnH-a8O2PE",
+    authDomain: "matchmaid-website.firebaseapp.com",
+    databaseURL: "https://matchmaid-website.firebaseio.com",
+    projectId: "matchmaid-website",
+    storageBucket: "matchmaid-website.appspot.com",
+    messagingSenderId: "802016119887",
+    appId: "1:802016119887:web:c25d0ae18b86f3a5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
